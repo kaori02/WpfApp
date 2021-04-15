b67844e95c018ec0c4b71ab03a71d8e6a90bcf75
kaori02 <kaorigaming02@gmail.com>
2021-04-15 16:12:44 +0700

    Add project files.

---
+++ WpfContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace WpfApp2
+{
+    class WpfContext : DbContext
+    {
+        public DbSet<ItemPenjualan> DaftarItemPenjualan { get; set; }
+    }
+}
