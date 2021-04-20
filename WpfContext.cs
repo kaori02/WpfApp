4c5734e95db7e5c96ed7ec468c2449c76aab89fe
kaori02 <kaorigaming02@gmail.com>
2021-04-20 07:28:46 +0700

    finale

--- WpfContext.cs
+++ WpfContext.cs
@@ -4,9 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.Entity;
+using MySql.Data.EntityFramework;
 
 namespace WpfApp2
 {
+    [DbConfigurationType(typeof(MySqlEFConfiguration))]
     class WpfContext : DbContext
     {
         public DbSet<ItemPenjualan> DaftarItemPenjualan { get; set; }
